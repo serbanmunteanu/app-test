e5d51f882486e389dac3d936bec8c0833b249740
Munteanu Serban <serban.munteanu1211@gmail.com>
2024-01-03 15:21:18 +0200

    retry

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from newww2!111444444';
+    return 'Latest deployment up';
   }
 }
