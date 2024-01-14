795f5665db09b14c254ed84258e7f47281ddfba7
Munteanu Serban <serban.munteanu1211@gmail.com>
2024-01-14 18:03:45 +0200

    update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as process from 'process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Testingggg ci/cd';
+    return `${process.env.SERBYX} - Testingggg ci/cd - 2`;
   }
 }
