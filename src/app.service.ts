53fedaeba6877d6daa24e8eab99c006e1b5a52b9
Munteanu Serban <serban.munteanu1211@gmail.com>
2024-01-16 17:56:53 +0200

    test ci/cd

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
   constructor(private readonly prismaService: PrismaService) {}
 
   getHello(): string {
-    return `Hello World! bombumbom`;
+    return `Hello World!`;
   }
 
   async getStatus(): Promise<any> {
